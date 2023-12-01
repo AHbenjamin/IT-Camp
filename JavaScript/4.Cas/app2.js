09f632c4ba8a93efe796cd1f5bbc5ad8519e4b89
AHbenjamin <bendziii21@gmail.com>
2023-12-01 20:04:21 +0100

    4.cas

---
+++ JavaScript/4.Cas/app2.js
@@ -0,0 +1,44 @@
+// OPERATORI U JS //
+
+//  Postoji 6 razlicitih operatora u JS:
+
+//  1. Aritmeticki operatori (Arithmetic operators)
+//  2. Operatori dodele vrednosti (Assignment operators)
+//  3. String operatori
+//  4. Operatori poredjenja (Comparison operators)
+//  5. Logicki operatori (Logical operators)
+//  6. Tipovni operatori (Type operators)
+
+//  1.1. + Addition:
+console.log(5 + 5);
+
+//  1.2. - SUbtraction:
+console.log(7 - 3);
+
+//  1.3. * Multiplication:
+console.log(3 * 5);
+
+//  1.4. / Division:
+console.log(10 / 2);
+
+//  1.5. ** Exponentation:
+console.log(2 ** 3);
+
+//  1.6. % Modul (ostatak pri deljenju):
+console.log(12 % 5);
+
+// Da bi broj bio paran mora ostatak pri deljenju sa 2 biti 0.
+// broj % 2 mora biti 0
+
+// Da bi broj bio neparan mora ostatak pri deljenju sa 2 biti razlicit od 0.
+//  broj % 2 mora biti razlicit od 0
+
+//  1.7. ++ Increment:
+let a = 13;
+a++;
+console.log(a);
+
+//  1.7. -- Decrement:
+let b = 13;
+b--;
+console.log(b);
