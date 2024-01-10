b29a80902541c5c1fedd02241dd24f7674e453ea
AHbenjamin <bendziii21@gmail.com>
2024-01-11 00:02:40 +0100

    DZENO

--- HTML-CSS/DZENO/app.js
+++
@@ -1,7 +0,0 @@
-var button = document.createElement("button");
-button.textContent = "Click me";
-button.addEventListener("click", displayMessage);
-document.body.appendChild(button);
-function displayMessage() {
-    alert("Dzeno Hvala sto si nas ostavio");
-}
